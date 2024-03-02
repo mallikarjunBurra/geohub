0ad2f771f02bf9bad3892d7ee4d22568643d6123
mallikarjunBurra <mallikarjun9515@gmail.com>
2024-03-02 14:07:55 +0000

    solution

---
+++ src/main/java/com/example/geohub/repository/CityRepository.java
@@ -0,0 +1,28 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * java.util.ArrayList;
+ * 
+ */
+
+// Write your code here
+package com.example.geohub.repository;
+
+import java.util.*;
+import com.example.geohub.model.City;
+import com.example.geohub.model.Country;
+
+public interface CityRepository {
+    ArrayList<City> getCities();
+
+    City getCityById(int cityId);
+
+    City addCity(City city);
+
+    City updateCity(int cityId, City city);
+
+    void deleteCity(int cityId);
+
+    Country getCountry(int cityId);
+}
